3d680e5efd6b5a24c2b05c833d8776a47d0e6eba
Deva Dathan <devadathan21@gmail.com>
2023-12-03 03:22:02 +0530

    new

--- landing_page.php
+++ landing_page.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/x-icon" href="assets/images/school_logo.png">
     <style>
     body {
       margin: 0;
@@ -22,7 +22,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>SCHOOL MANAGEMENT SYSTEM</title>
   </head>
   <body>
 
